a9e1d90a5e1274e5947bbed9cf318e1aaf63d53a
Asanka Dharma <mowgl1@Asankas-Mac-Pro.local>
2017-07-25 21:44:34 -0700

    finished string2.py exercises

--- google-python-exercises/basic/string2.py
+++ google-python-exercises/basic/string2.py
@@ -49,8 +49,20 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
+  if len(a) % 2 == 0:
+    a_front = a[:((len(a)/2))]
+    a_back = a[((len(a)/2)):]
+  else:
+    a_front = a[:((len(a)+1)/2)]
+    a_back = a[((len(a)+1)/2):]
+  if len(b) % 2 == 0:
+    b_front = b[:((len(b)/2))]
+    b_back = b[((len(b)/2)):]
+  else:
+    b_front = b[:((len(b)+1)/2)]
+    b_back = b[((len(b)+1)/2):]
   # +++your code here+++
-  return
+  return a_front + b_front + a_back + b_back
 
 
 # Simple provided test() function used in main() to print
